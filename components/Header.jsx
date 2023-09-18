d2c4c675e0ff743f856551c4996895b581d0939f
Nanguti <g.nanguti@gmail.com>
2023-09-18 15:26:32 +0300

    Change home layout

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,46 +1,74 @@
-"use client";
-import React, { useState } from "react";
-import NavbarItem from "./NavbarItem";
-
-function MobileMenuButton({ isOpen, toggleMenu }) {
-  return (
-    <button
-      className="text-white focus:outline-none md:hidden"
-      onClick={toggleMenu}
-    >
-      {isOpen ? "X" : "Menu"}
-    </button>
-  );
-}
-
 function Header() {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
-  };
-
   return (
-    <nav className="bg-white p-4">
-      <div className="container mx-auto flex justify-between items-center">
-        <div className="flex items-center">
-          <img src="logo.png" alt="Logo" className="w-10 h-10 mr-2" />
-          <span className="text-gray-800 text-xl font-semibold">
-            TechMaster
-          </span>
-          {/* Add the search bar here */}
-          <input
-            type="text"
-            placeholder="Search..."
-            className="bg-white text-gray-800 px-4 py-2 ml-4 rounded-full search-border"
-          />
+    <nav className="fixed z-50 w-full bg-white top-0 flex flex-wrap items-center justify-between px-2 py-3 shadow-lg">
+      <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
+        <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
+          <a
+            className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-slate-700"
+            href="/learning-lab/tailwind-starter-kit/presentation"
+          >
+            Tailwind Starter Kit
+          </a>
+          <button
+            className="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+            type="button"
+          >
+            <i className="fas fa-bars" />
+          </button>
         </div>
-        <MobileMenuButton isOpen={isOpen} toggleMenu={toggleMenu} />
-        <div className={`md:flex ${isOpen ? "block" : "hidden"} mt-4 md:mt-0`}>
-          <NavbarItem text="Home" link="/" />
-          <NavbarItem text="Blog" link="/blog" />
-          <NavbarItem text="About" link="/about" />
-          {/* Add more menu items as needed */}
+        <div
+          className="lg:flex flex-grow items-center hidden"
+          id="example-navbar-danger"
+        >
+          <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
+            <li className="nav-item">
+              <a
+                href="https://www.facebook.com/sharer/sharer.php?u=https://www.creative-tim.com/learning-lab/tailwind-starter-kit#/presentation"
+                className="px-3 py-2 flex items-center text-xs uppercase font-bold text-slate-700 hover:text-slate-500"
+                target="_blank"
+              >
+                <i className="fab fa-facebook-square text-lg leading-lg text-slate-400" />
+                <span className="ml-2">Share</span>
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                href="https://twitter.com/intent/tweet?text=Check%20Tailwind%20Starter%20Kit%2C%20a%20beautiful%20extension%20of%20%23TailwindCSS%20made%20by%20%40CreativeTim%20%23webdesign%20%23starterkit&url=https%3A%2F%2Fwww.creative-tim.com%2Flearning-lab%2Ftailwind-starter-kit%23%2Fpresentation"
+                className="px-3 py-2 flex items-center text-xs uppercase font-bold text-slate-700 hover:text-slate-500"
+                target="_blank"
+              >
+                <i className="fab fa-twitter text-lg leading-lg text-slate-400" />
+                <span className="ml-2">Tweet</span>
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                className="px-3 py-2 flex items-center text-xs uppercase font-bold text-slate-700 hover:text-slate-500"
+                href="/learning-lab/tailwind-starter-kit/documentation/quick-start"
+              >
+                <i className="far fa-file-alt text-lg leading-lg text-slate-400" />
+                <span className="ml-2">Docs</span>
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                className="download-button px-3 py-2 flex items-center text-xs uppercase font-bold text-slate-700 hover:text-slate-500"
+                href="/learning-lab/tailwind-starter-kit/documentation/download"
+              >
+                <i className="fas fa-arrow-alt-circle-down text-lg leading-lg text-slate-400" />
+                <span className="ml-2">Download</span>
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                href="https://github.com/creativetimofficial/tailwind-starter-kit/archive/designer-files.zip?ref=tsk-navbar"
+                className="download-button px-3 py-2 flex items-center text-xs uppercase font-bold text-slate-700 hover:text-slate-500"
+              >
+                <i className="fab fa-sketch text-lg leading-lg text-slate-400" />
+                <span className="ml-2">Download Designer</span>
+              </a>
+            </li>
+          </ul>
         </div>
       </div>
     </nav>
